38583d911ca4eaa616f1902f825d7f67d5d5c859
Ryan Keys <r.keys1998@gmail.com>
2020-01-23 14:52:25 -0800

    added reverse sentence and reverse word fns

--- rearrange.py
+++ rearrange.py
@@ -9,5 +9,22 @@ def rearrange(sentence):
     return ' '.join(words)
 
 
+def reverse_sentence(sentence):
+    return " ".join(sentence.split(" ")[::-1])
+
+
+def reverse_words(sentence):
+    word_list = []
+    words = sentence.split(" ")
+    for word in words:
+        word = word[::-1]
+        word_list.append(word)
+    output = " ".join(word_list)
+    return output
+
+
 if __name__ == "__main__":
+    sentence = "how now brown cow"
     print(rearrange(sentence))
+    print(reverse_sentence(sentence))
+    print(reverse_words(sentence))
