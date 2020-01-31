a78474832b4ff75acc2be0b05e69426f825510fc
Ryan Keys <r.keys1998@gmail.com>
2020-01-30 20:54:14 -0800

    added Procfile

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from histogram import histogram
+from histogram import weighted_histogram
 app = Flask(__name__)
 source_text = "test.txt"
-histo = str(histogram(source_text))
+histo = str(weighted_histogram(source_text))
 
 
 @app.route('/')
